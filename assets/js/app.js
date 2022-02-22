d1dda94baceb18dc81042303f7f91508a9659039
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-02-21 19:46:16 -0600

    started another way of building this project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.getElementById(#generate).addEventListener('click', function (){
+  var length = parseInt(prompt('choose the length of your passwrod between 8 and 128'));
+  var hasLower = prompt('do you want lower case letters?');
+  var hasUpper = prompt('do you want lower case letters?');
+  var hasNumber = prompt('do you want lower case letters?');
+  var hasSymbole = prompt('do you want lower case letters?');
+  // proccess user input or user desire 
+  var pw = generatePassword(length, hasLower, hasUpper, hasSymbole, hasNumber);
+  // returne output
+  var password = document.getElementById('password');
+  password.innerHTML = pw;
+})
 
-
-//creat a generatPassword function
-function generatePassword (passwordArray, password) {
-
-  var passwordArray = [];
-  var array = '';
-   
-  // check the length
-  var length = parseInt(prompt('choose the length of your password between 8 and 128'));
-  while(true){
-    if(length >= 8 && length <=128 ){
-      alert ('you choosed ' +  length)
-    }
-    else{
-    alert ('please choose the right number')
-    break;
-   }
-
-
+// Add event listener to generate button
+function generatePassword(length, hasLower, hasUpper, hasSymbole, hasNumber){
   
-  // see if they want lower case
-  var hasLower = confirm('do you want to include a lower case')
-
-  // see if they want to include an upper case
-  var hasUpper = confirm('do you want to include a upper case')
-    
-  // see if they want to inclkude a number 
-  var number = confirm('do you want to includ a number')
-
 }
-
-var passwordArray = [];
-  if(hasLower){
-    passwordArray = passwordArray.concat(String.fromCharCode(Math.floor (Math.random() * 26) + 97));
-  }
-
-  if (hasUpper){
-    passwordArray = passwordArray.concat(getRandomUpper());
-  }
-
-  if (number){
-    passwordArray = passwordArray.concat(number());
-  }
-  }
-
-
 // Write password to the #password input
 function writePassword() {
   // var password = generatePassword();
@@ -57,23 +26,21 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
-function getRandomLower(){
-  return String.fromCharCode(Math.floor (Math.random() * 26) + 97);
-}
+// function getRandomLower(){
+//   return String.fromCharCode(Math.floor (Math.random() * 26) + 97);
+// }
 
- function getRandomUpper(){
-  return String.fromCharCode(math.floor (math.random() * 26) + 48);
-}
+//  function getRandomUpper(){
+//   return String.fromCharCode(math.floor (math.random() * 26) + 48);
+// }
 
-function number(){
-  return String.fromCharCode(math.floor (math.random() * 10) +48);
-}
+// function number(){
+//   return String.fromCharCode(math.floor (math.random() * 10) +48);
+// } 
 
-function symbols(){
-  var symbols = '#%^&*()(*&^%)'
-  return symbols[math.floor (math.random() * symbols.length)];
-} 
+// function symbols(){
+//   var symbols = '#%^&*()(*&^%)'
+//   return symbols[math.floor (math.random() * symbols.length)];
+// } 
