dbf630644a2cbcd92e638ebda26f0d91ea89c11b
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-02-24 00:42:20 -0600

    added the genera

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,68 @@
-// Assignment Code
-document.getElementById('generate').addEventListener('click', function (){
-  var length = parseInt(prompt('choose the length of your passwrod between 8 and 128'));
-  var hasLower = prompt('do you want lower case letters?');
-  var hasUpper = prompt('do you want lower case letters?');
-  var hasNumber = prompt('do you want lower case letters?');
-  var hasSymbole = prompt('do you want lower case letters?');
-  // proccess user input or user desire 
-  var pw = generatePassword(length, hasLower, hasUpper, hasSymbole, hasNumber);
-  // returne output
-  document.getElementById('password').innerHTML = pw;
+var password = document.getElementById('password');
+var generateBtn = document.getElementById('generate');
 
-})
+function generatedPassword (hasLower, hasUpper, hasNumber, hasSymbole, length) {
 
-// Add event listener to generate button
-function generatePassword(length, hasLower, hasUpper, hasSymbole, hasNumber){
-  if (length >= 128 && length <= 8){
-  alert('length should be at less than 128 and greater than 8');
-  return
-  } 
+  // setting up an emty string to add the values to it
+  let box = '';
 
-  let pool = '';
-
-  if(hasLower == 'yes' || hasLower == 'YES'){
-    let lowerChars = 'qwertyuipoasdfghjklmnbvczx'
-    pool += lowerChars;  //the += will add the value on the right to the variable on the left
+  // reload the page after user dind't choose the right length
+  if(length < 8 || length > 128){
+    alert('please choose the right number');
+    window.location.reload();
   }
 
-  if(hasUpper ==  'yes'|| hasUpper == 'YES'){
-    let upperChars = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-    pool += upperChars;  //the += will add the value on the right to the variable on the left
-  }
+  else{
+    
+  // Get random lower
+  var lower = 'qwertyuiopasdfghjklzxcvbnm';
+  hasLower = String.fromCharCode(Math.floor(Math.random() * lower.length) + 97 );
+  box += hasLower;
 
-  if(hasNumber ==  'yes' || hasNumber == 'YES'){
-    let numberChars = '0123456789'
-    pool += numberChars;  //the += will add the value on the right to the variable on the left
-  }
 
-  if(hasSymbole ==  'yes' || hasNumber == 'YES'){
-    let lowerChars = '!@#$%^&*()_+?><'
-    pool += lowerChars;  //the += will add the value on the right to the variable on the left
-  }
+  // Get random upper
+  var upper = 'QWERTYUIOPASDFGHJKLMNBVCXZ';
+  hasUpper = String.fromCharCode(Math.floor(Math.random() * upper.length) + 97 ); // used the same number form the chartCode and converted it to an upperCase on line 16
+  // same then doing string.fromChartCode(Math.floor(Math.random()* upper.length) + 65) 
+  hasUpper = hasUpper.toUpperCase();
+  box += hasUpper;
+  console.log (box);
 
-  let result = "";
-  for(var i =0 ; i < length; i++){
-  let min = result.length;
-  let max = pool.length;
-  let randomIndex = Math.floor(Math.random() * (max - min) + min)
-     let newChar = pool.charAt(randomIndex);
-     result += newChar;
+
+  //get random number
+  var number = '0123456789';
+  hasNumber = Math.floor(Math.random() * number.length);
+  box += hasNumber;
+
+  //Get random symbole 
+  var symboles = '!@#$%^&*()_+?><';
+  hasSymbole = symboles[Math.floor(Math.random() * symboles.length)];
+  box += hasSymbole;
+  console.log (box);
+
+  // set a result emty string to add the values until we hit the length 
+  let result = '';
+  while(result.length < length){
+    
+    let randomIndex = Math.floor(Math.random() * result.length);
+    let newRandom = box.charAt(randomIndex);
+     result += newRandom //box.charAt(Math.floor(Math.random() * length));
+  }
+  
+ console.log (result);
+ return result;
 }
-
-return result
 }
+ 
+
+
+generateBtn.addEventListener('click', function(){
+  var length = prompt('please try choose a number between 8 and 128');
+  var hasLower = confirm ('Do you want to include a lower case letter?');
+  var hasUpper = confirm ('Do you want to include a lower case letter?')
+  var hasSymbole = confirm ('Do you want to include a lower case letter?')
+  var hasNumber = confirm ('Do you want to include a lower case letter?')
+
+  pw = generatedPassword(hasLower, hasUpper, hasNumber, hasSymbole, length);
+  password.textContent = pw;
+});
